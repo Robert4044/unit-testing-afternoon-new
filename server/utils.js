8dafc7c2b6b6495d03708bffbeb1842006f1faad
Robert Martin <robert4044@gmail.com>
2020-12-17 07:48:52 -0600

    complete

--- server/utils.js
+++ server/utils.js
@@ -1,20 +1,22 @@
 module.exports = {
-  wordCount(posts) {
-    return posts.reduce(
-      (accum, post) => (accum += post.text.split(' ').length),
-      0,
-    );
-  },
-  attachUserName(users, posts) {
-    let userDict = users.reduce((accum, user) => {
-      accum[user.id] = user;
-      return accum;
-    }, {});
-    return posts.map(post => {
-      post.displayName = `${userDict[post.userId].first} ${
-        userDict[post.userId].last
-      }`;
-      return post;
-    });
-  },
-};
+    wordCount(posts) {
+        return posts.reduce(
+            (accum, post) => (accum += post.text.split(' ').length),
+            0
+        )
+    },
+    attachUserName(users, posts) {
+        let userDict = users.reduce((accum, user) => {
+            accum[user.id] = user
+            return accum
+        }, {})
+        return posts
+            .filter(post => userDict[post.userId])
+            .map(post => {
+                post.displayName = `${userDict[post.userId].first} ${
+                    userDict[post.userId].last
+                }`
+                return post
+            })
+    },
+}
